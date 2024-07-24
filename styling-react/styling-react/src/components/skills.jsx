aff4292b4180138551ab996a2e63c04367ad1753
Cervin <houcervin55@gmail.com>
2024-07-24 14:04:24 +0700

    update service description

--- styling-react/styling-react/src/components/skills.jsx
+++ styling-react/styling-react/src/components/skills.jsx
@@ -6,10 +6,10 @@ import shop from '../images/ecommerce.svg'
 export default function skills(){
     return(
         <div className="flex gap-4 mt-40 flex-wrap min-w-1/2 justify-center">
-            <Card icon={computer} title='Web Design' description='Transforming ideas to visually stunning digital experience'/>
-            <Card icon={mobile} title='Web Design' description='Transforming ideas to visually stunning digital experience'/>
-            <Card icon={wordpress} title='Web Design' description='Transforming ideas to visually stunning digital experience'/>
-            <Card icon={shop} title='Web Design' description='Transforming ideas to visually stunning digital experience'/>
+            <Card icon={computer} title='Web Development' description='Transforming ideas to visually stunning digital experience'/>
+            <Card icon={mobile} title='Mobile Development' description='Enhancing user engagement with seamless and intuitive design'/>
+            <Card icon={wordpress} title='Wordpres Web Builder' description='Evolate your ideas quickly and amaze your desire'/>
+            <Card icon={shop} title='Eccomerce Website' description='Just take in easy your shop, create your own market'/>
         </div>
     )
 }
